8a15a267b86c6b645b5d107858be44458b943fa7
Erhan Gündüz <erhangndz@live.com>
2023-10-10 12:11:22 +0300

    updated

---
+++ CarBook.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
