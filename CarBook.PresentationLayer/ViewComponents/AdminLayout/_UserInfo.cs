a896c4b180fa37ea75b6b8c3131276a7302fe0b4
Erhan Gündüz <erhangndz@live.com>
2023-11-02 17:18:25 +0300

    updated

---
+++ CarBook.PresentationLayer/ViewComponents/AdminLayout/_UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using CarBook.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.PresentationLayer.ViewComponents.AdminLayout
+{
+	public class _UserInfo:ViewComponent
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public _UserInfo(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var user = await _userManager.FindByNameAsync(User.Identity.Name);
+			ViewBag.name = user.NameSurname;
+
+			return View();
+		}
+	}
+}
