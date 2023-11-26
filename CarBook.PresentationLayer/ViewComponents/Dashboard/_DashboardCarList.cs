82fdd4b63289d563bba35c959dedc880f7b65f59
Erhan Gündüz <erhangndz@live.com>
2023-11-26 18:30:23 +0300

    updated

---
+++ CarBook.PresentationLayer/ViewComponents/Dashboard/_DashboardCarList.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CarBook.BusinessLayer.Abstract;
+using CarBook.DtoLayer.DTOs.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.PresentationLayer.ViewComponents.Dashboard
+{
+    public class _DashboardCarList:ViewComponent
+    {
+        private readonly ICarService _carService;
+        private readonly IMapper _mapper;
+
+        public _DashboardCarList(ICarService carService, IMapper mapper)
+        {
+            _carService = carService;
+            _mapper = mapper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cars = _carService.TGetAll();
+            var values = _mapper.Map<List<CarDto>>(cars);
+            return View(values);
+        }
+    }
+}
